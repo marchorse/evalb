a88d6505cdfbbb43a3852bdaf0e6511b58a6438f
Marchorse <realer111@qq.com>
2014-09-12 14:33:24 +0800

    Update syntree.py

--- syntree.py
+++ syntree.py
@@ -4,10 +4,22 @@ class TreeNode(object):
     def __init__(self):
         self.label=None
         self.node_type=None
+        self.word=None
+        
+    node_types=['terminal','constituent','pos']
 
 class Tree(object):
-    def __init__(self):
+    def __init__(self, delimiters=None, bracket_sym=None):
+        if not delimiters:
+            self.delimiters=[' ', '\t']
+            self.bracket_sym=['(',')']
+            self.bracket_sym_start=self.bracket_sym[0]
+            self.bracket_sym_end=self.bracket_sym[1]
+
+    def parse_brackets_string(self, brackets_string):
+        
         pass
 
-    def read_brackets(self, brackets):
+    def parse_brackets_string_helper(self, root, brackets_string):
+        
         pass
