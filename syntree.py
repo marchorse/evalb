344090753d1f6c04b7e489edad99e3d5086ade7d
Marchorse <marchorse.zhang@gmail.com>
2014-09-12 08:43:02 +0000

    fix

---
+++ syntree.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class TreeNode(object):
+    def __init__(self):
+        self.label=None
+        self.node_type=None
+
+class Tree(object):
+    def __init__(self):
+        pass
+
+    def read_brackets(self, brackets):
+        pass
